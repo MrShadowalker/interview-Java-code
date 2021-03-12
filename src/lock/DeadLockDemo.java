8ef131ec98b4c21c0c1bc821f78244eeac26e506
MrShadowalker <mr.shadowalker@foxmail.com>
2021-03-12 10:31:40 +0800

    死锁

---
+++ src/lock/DeadLockDemo.java
@@ -0,0 +1,53 @@
+package lock;
+
+import java.util.concurrent.TimeUnit;
+
+public class DeadLockDemo {
+    public static void main(String[] args) {
+        String lockA = "lockA";
+        String lockB = "lockB";
+
+        new Thread(new HoldLockThread(lockA, lockB), "ThreadA").start();
+        new Thread(new HoldLockThread(lockB, lockA), "ThreadB").start();
+
+        // jps -l
+        // jstack 线程数
+    }
+}
+
+class HoldLockThread implements Runnable {
+    private String lockA;
+    private String lockB;
+
+    public HoldLockThread(String lockA, String lockB) {
+        this.lockA = lockA;
+        this.lockB = lockB;
+    }
+
+
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+        synchronized (lockA) {
+            System.out.println(Thread.currentThread().getName() + "\t 自己持有：" + lockA + "\t 尝试获得：" + lockB);
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            synchronized (lockB) {
+                System.out.println(Thread.currentThread().getName() + "\t 自己持有：" + lockA + "\t 尝试获得：" + lockB);
+            }
+        }
+    }
+}
