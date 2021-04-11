d7087fac91a28f9fbb586c08e55142dbdaf939e4
MrShadowalker <mr.shadowalker@foxmail.com>
2021-04-11 11:59:47 +0800

    数组算法题： 移动 0 移除元素

---
+++ src/algorithm/array/MoveZeroesDemo.java
@@ -0,0 +1,68 @@
+package algorithm.array;
+
+/**
+ * Leetcode 283
+ * 移动零
+ * <p>
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 说明:
+ * <p>
+ * 必须在原数组上操作，不能拷贝额外的数组。
+ * 尽量减少操作次数。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/move-zeroes
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Shadowalker
+ */
+public class MoveZeroesDemo {
+    public static void main(String[] args) {
+        int[] req = {0, 1, 0, 3, 12};
+        // moveZeroes1(req);
+        moveZeroes2(req);
+        for (int i : req) {
+            System.out.println(i);
+        }
+    }
+
+    // 双指针
+    private static void moveZeroes2(int[] nums) {
+        int left = 0;
+        int right = 0;
+        while (right < nums.length) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    private static void swap(int[] nums, int left, int right) {
+        int tmp;
+        tmp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = tmp;
+    }
+
+    // 较省空间但是时间复杂度很蠢的
+    public static void moveZeroes1(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[j] != 0) {
+                        nums[i] = nums[j];
+                        nums[j] = 0;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
